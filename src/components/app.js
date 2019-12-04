4a7a0fae4d75e0c87d8ace07d1d3f6e0e29f5970
Kris <kriskuiper74@gmail.com>
2019-12-04 15:43:18 +0100

    feat: add background and invert functionality

--- src/components/app.js
+++ src/components/app.js
@@ -18,38 +18,54 @@ class App extends Component {
 		this.setState({ imagesToRender: alteredImagesToRender })
 	}
 
+	handleInvert = () => {
+		const { imagesToRender } = this.state
+		const alteredImagesToRender = imagesToRender.map(image => {
+			return { ...image, isShown: !image.isShown }
+		})
+
+		this.setState({ imagesToRender: alteredImagesToRender })
+	}
+
 	render({}, { imagesToRender }) {
 		return (
 			<div id="app">
 				<main class="layout">
 					<div class="app-image-container">
+						<img src="/assets/images/background.png" class="app-image" />
 						{imagesToRender.map(image => (
 							image.isShown
-								? <img src={`/assets/images/${image.name}.png`} class="app-image" />
+								? <img src={`/assets/images/${image.name}.png`} class="app-image app-image--is-vague" />
 								: ''
 						))}
+						<img src="/assets/images/gebied-namen.png" class="app-image" />
 					</div>
 					<div class="app-form-container">
-					<form class="app-form">
-						{imagesToRender.map((image, i) => (
-							<div class="app-form__input-group">
-								<input
-									class="app-form__checkbox"
-									type="checkbox"
-									value={image.name}
-									id={`name-${i}`}
-									onChange={this.handleFormChange}
-									checked={image.isShown}
-								/>
-								<label
-									class="app-form__label"
-									for={`name-${i}`}
-								>
-									{image.name}
-								</label>
-							</div>
-						))}
-					</form>
+						<form class="app-form">
+							{imagesToRender
+								.filter(image => image.name !== 'rustplekken')
+								.map((image, i) => (
+								<div class="app-form__input-group">
+									<input
+										class="app-form__checkbox"
+										type="checkbox"
+										value={image.name}
+										id={`name-${i}`}
+										onChange={this.handleFormChange}
+										checked={image.isShown}
+									/>
+									<label
+										class="app-form__label"
+										for={`name-${i}`}
+									>
+										{image.label}
+									</label>
+								</div>
+							))}
+						</form>
+						<button onClick={this.handleInvert}>
+							invert
+						</button>
 					</div>
 				</main>
 			</div>
--- src/components/img-names.js
+++ src/components/img-names.js
@@ -1,9 +1,9 @@
 export default [
-	{ name: 'geluid-verkeer-1', isShown: true },
-	{ name: 'geluid-verkeer-2', isShown: true },
-	{ name: 'honden-uitlaat-plekken', isShown: true },
-	{ name: 'markten', isShown: true },
-	{ name: 'sportplekken-openbaar', isShown: true },
-	{ name: 'studenten-huisvesting', isShown: true },
-	{ name: 'gebied-namen', isShown: true }
+	{ name: 'rustplekken', isShown: false, label: null },
+	{ name: 'geluid-verkeer-1', isShown: true, label: 'Verkeer' },
+	{ name: 'geluid-verkeer-2', isShown: true, label: 'Nog meer verkeer' },
+	{ name: 'honden-uitlaat-plekken', isShown: true, label: 'Blaffende honden' },
+	{ name: 'markten', isShown: true, label: 'Markten' },
+	{ name: 'sportplekken-openbaar', isShown: true, label: 'Schreeuwende, sportende kinderen' },
+	{ name: 'studenten-huisvesting', isShown: true, label: 'Feestende studenten' },
 ]
