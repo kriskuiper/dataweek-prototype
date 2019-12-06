19b73ef4a4013b98440f3e80dad7cc3654cc61da
Kris <kriskuiper74@gmail.com>
2019-12-06 15:43:44 +0100

    fix: change names

--- src/components/img-names.js
+++ src/components/img-names.js
@@ -1,7 +1,7 @@
 export default [
 	{ name: 'rustplekken', isShown: false, label: null, hasExtraClass: true },
-	{ name: 'geluid-verkeer-1', isShown: true, label: 'Treinen en metros' },
-	{ name: 'geluid-verkeer-2', isShown: true, label: 'Trams' },
+	{ name: 'geluid-verkeer-1', isShown: true, label: 'Openbaar vervoer' },
+	{ name: 'geluid-verkeer-2', isShown: true, label: 'Autoverkeer' },
 	{ name: 'honden-uitlaat-plekken', isShown: true, label: 'Hondenparken' },
 	{ name: 'markten', isShown: true, label: 'Marktplaatsen' },
 	{ name: 'sportplekken-openbaar', isShown: true, label: 'Openbare sportplekken' },
